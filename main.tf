53258c96d2de136fe10f95251c6017df62638938
Razvan Cranganu <razvan.cranganu@wundermanthompson.com>
2021-11-24 22:45:34 +0000

    Added IAM policy

--- main.tf
+++ main.tf
@@ -3,6 +3,11 @@ module "route53" {
   domain_name = var.domain_name
 }
 
+module "iam_policy" {
+  source         = "./module/iam_policy"
+  s3_bucket_name = "${terraform.workspace}-${var.bucket_name}"
+}
+
 module "s3" {
   source      = "./module/s3"
   bucket_name = "${terraform.workspace}-${var.bucket_name}"
